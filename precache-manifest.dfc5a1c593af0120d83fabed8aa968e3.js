e01bbee981f55b90f90985831ac54d160e19222d
Евгения Никонова <begale-dog@mail.ru>
2021-11-14 03:27:54 +0300

    Updates

---
+++ precache-manifest.dfc5a1c593af0120d83fabed8aa968e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7bea472b593b6ef305365a637a94315",
+    "url": "/test-applab-native/index.html"
+  },
+  {
+    "revision": "8325b417dfb98700c42d",
+    "url": "/test-applab-native/static/js/2.247d4750.chunk.js"
+  },
+  {
+    "revision": "a99ae5b1c934942978fdbf9d6529b918",
+    "url": "/test-applab-native/static/js/2.247d4750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863edaaf48bc4ce66b7e",
+    "url": "/test-applab-native/static/js/main.e959340d.chunk.js"
+  },
+  {
+    "revision": "1c30a7553cb5e45e1fd1",
+    "url": "/test-applab-native/static/js/runtime-main.44a1291c.js"
+  },
+  {
+    "revision": "b9e134d3b26f4d54299de0ff00465db9",
+    "url": "/test-applab-native/static/media/checkMark.b9e134d3.svg"
+  }
+]);
